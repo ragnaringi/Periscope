1e063404bca9192e0e5e3f7a2ee51ac66b1de20b
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2016-05-31 22:43:55 +0100

    Added ofxDatGui download to setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/kylemcdonald/ofxCv.git openFrameworks/addons/ofxCv
+git clone https://github.com/braitsch/ofxDatGui.git openFrameworks/addons/ofxDatGui
