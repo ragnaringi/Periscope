c40353e75fd91555b5bb03e1cf875c0a00f6b6ab
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2016-05-31 23:18:35 +0100

    Updated setup script to copy ofxDatGui resources

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 
 git clone https://github.com/kylemcdonald/ofxCv.git openFrameworks/addons/ofxCv
 git clone https://github.com/braitsch/ofxDatGui.git openFrameworks/addons/ofxDatGui
+mkdir Periscope/bin/data/ofxbraitsch
+cp -r openFrameworks/addons/ofxDatGui/ofxbraitsch/* Periscope/bin/data/ofxbraitsch
