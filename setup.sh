045d54a289b89b2240a7ba85046736cd889a9497
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2016-07-07 16:59:35 +0100

    Removed unused third party libs

--- setup.sh
+++ setup.sh
@@ -2,8 +2,5 @@
 
 git clone https://github.com/kylemcdonald/ofxCv.git openFrameworks/addons/ofxCv
 git clone https://github.com/kylemcdonald/ofxCcv.git openFrameworks/addons/ofxCcv
-git clone https://github.com/braitsch/ofxDatGui.git openFrameworks/addons/ofxDatGui
-mkdir Periscope/bin/data/ofxbraitsch
-cp -r openFrameworks/addons/ofxDatGui/ofxbraitsch/* Periscope/bin/data/ofxbraitsch
 cd openFrameworks/addons/ofxCcv
 ./setup.sh
