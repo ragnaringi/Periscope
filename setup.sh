e1daa7178a6108ec57baf9997304ca571d463c44
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2016-07-24 18:19:47 +0100

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/jefftimesten/ofxJSON.git openFrameworks/addons/ofxJSON
 git clone https://github.com/kylemcdonald/ofxCv.git openFrameworks/addons/ofxCv
-git clone https://github.com/kylemcdonald/ofxCcv.git openFrameworks/addons/ofxCcv
-cd openFrameworks/addons/ofxCcv
-./setup.sh
+git clone https://github.com/jefftimesten/ofxJSON.git openFrameworks/addons/ofxJSON
+git clone https://github.com/astellato/ofxSyphon.git openFrameworks/addons/ofxSyphon
+git clone https://github.com/elliotwoods/ofxAddonLib openFrameworks/addons/ofxAddonLib
+git clone https://github.com/elliotwoods/ofxSpout.git openFrameworks/ofxSpout
