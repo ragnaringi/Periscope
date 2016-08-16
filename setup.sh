6dc5598269f5623c888c9d8232b565fa5301bf9e
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2016-08-16 23:06:56 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ git clone https://github.com/kylemcdonald/ofxCv.git openFrameworks/addons/ofxCv
 git clone https://github.com/jefftimesten/ofxJSON.git openFrameworks/addons/ofxJSON
 git clone https://github.com/astellato/ofxSyphon.git openFrameworks/addons/ofxSyphon
 git clone https://github.com/Kj1/ofxSpout2.git openFrameworks/addons/ofxSpout2
+git clone https://github.com/elliotwoods/ofxBlackmagic2 openFrameworks/addons/ofxBlackmagic2
